ab0651d3007834ce2e58394250e483bfd8e60545
Lian Arthofer <77974438+LianArthofer@users.noreply.github.com>
2025-05-04 18:23:30 +0100

    nigga fix

--- components/date-range-picker.tsx
+++ components/date-range-picker.tsx
@@ -9,7 +9,7 @@ import {
   Popover,
   PopoverContent,
   PopoverTrigger,
-} from "@/components/ui/popover";
+} from "../components/ui/popover";
 
 interface DateRangePickerProps {
   dateRange: DateRange;
