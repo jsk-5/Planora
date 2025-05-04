eaf993aeb00b44788ff082a8397c221b1814074b
Lian Arthofer <77974438+LianArthofer@users.noreply.github.com>
2025-05-04 17:40:17 +0100

    dawdaw

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -8,6 +9,14 @@ const nextConfig: NextConfig = {
     // your project has type errors.
     // !! WARN !!
     ignoreBuildErrors: true,
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "./"),
+    };
+
+    return config;
   },
 };
 
